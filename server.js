296d54c49e1fbf6575bed24d7c03f92700a7a821
mar <111649405+39726@users.noreply.github.com>
2023-03-30 17:58:19 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const WebSocket = require("ws");
 
+const server = new WebSocket.Server({ port: process.env.PORT || 8080 });
+
+server.on('connection', (s) => {
+    console.log('Client connected');
+
+    s.on('message', (message) => {
+        console.log(`Recieved message: ${message}`);
+        s.send(`Echo: ${message}`);
+    });
+
+    s.on('close', () => {
+        console.log(`Client disconnected.`);
+    });
+});
